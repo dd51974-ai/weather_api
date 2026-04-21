0c784b99b308081a15147b03b312ad20acd0ba1a
Ai Tsujikawa <tuji.kamome@gmail.com>
2026-04-21 23:59:57 +0900

    anyway, commit

--- weather/views.py
+++ weather/views.py
@@ -3,21 +3,32 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    api_key = "bdfd8d2deca36550b33f7c3dcd280007" # Your api_key
-    city = "Tokyo"
+    city = None
+    context = {}
 
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
-    response = requests.get(url)
+    if request.method == "POST":  # When formed to type city somewher.
+        city = request.POST['city']  # Enter to type city
+        api_key = ""
+        url = f"http:api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+        try:
+            response = requests.get(url)
+
+
+        data = response.json() # Get data of json
+
+        if response.status_code == 200: # Result after being post
+            context = {
+                "country": data["sys"]["country"],
+                "city": data["name"],
+                "temp": data["main"]["temp"],
+                "desc": data["weather"][0]["description"],
+            }
+    else: # spelling mistake
+        context = {
+            "error": "City not found"
+        }
 
     print("STATUS:", response.status_code)
     print("TEXT:", response.text)
 
-    data = response.json() # Get data of json
-
-    context = {
-        "city": data["name"],
-        "temp": data["main"]["temp"],
-        "desc": data["weather"][0]["description"],
-    }
-
-    return render(request, "weather/home.html", context)
+    return render(request, "weather/home.html", context) # Return to "home.html" as a result
