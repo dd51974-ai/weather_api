8b83da695bf5b3d61d8f226e7f3a2527c131ff26
Ai Tsujikawa <tuji.kamome@gmail.com>
2026-04-22 14:34:27 +0900

    write to add "http://" at 12 line

--- weather/views.py
+++ weather/views.py
@@ -8,27 +8,25 @@ def home(request):
 
     if request.method == "POST":  # When formed to type city somewher.
         city = request.POST['city']  # Enter to type city
-        api_key = ""
-        url = f"http:api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
-        try:
-            response = requests.get(url)
+        api_key = "bdfd8d2deca36550b33f7c3dcd280007"
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+        response = requests.get(url)
+        data = response.json()  # Get data of json
 
+        print("STATUS:", response.status_code)
+        print("TEXT:", response.text)
 
-        data = response.json() # Get data of json
-
-        if response.status_code == 200: # Result after being post
+        if response.status_code == 200:  # Result after being post
             context = {
                 "country": data["sys"]["country"],
                 "city": data["name"],
                 "temp": data["main"]["temp"],
                 "desc": data["weather"][0]["description"],
             }
-    else: # spelling mistake
-        context = {
-            "error": "City not found"
-        }
+        else: # spelling mistake
+            context = {
+                "error": "City not found"
+            }
 
-    print("STATUS:", response.status_code)
-    print("TEXT:", response.text)
 
     return render(request, "weather/home.html", context) # Return to "home.html" as a result
